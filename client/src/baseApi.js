4e3dbb5956d50d4d6625382d1f3cd57d7d5f3c8c
Manan <157731770+mananshah78424@users.noreply.github.com>
2024-08-26 19:59:46 -0700

    updating baseApi with render link

--- client/src/baseApi.js
+++ client/src/baseApi.js
@@ -2,8 +2,8 @@ import axios from "axios";
 console.log("Process env is", process.env.NODE_ENV);
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://instas3verse.onrender.com"
-    : "https://instas3verse.onrender.com";
+    ? "https://instas3verse.onrender.com/api/posts"
+    : "https://instas3verse.onrender.com/api/posts";
 
 export default axios.create({
   baseURL,
